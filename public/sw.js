5a4613fedec8591d2c600f2ea5f74b149aee7254
pabloserrazinads-source <pabloserrazinads@gmail.com>
2026-09-15 00:59:29 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pedevia-shell-v13248b';
+const CACHE='pedevia-shell-v13249csp';
 const SHELL=['/','/manifest.webmanifest','/pedevia-icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}));
